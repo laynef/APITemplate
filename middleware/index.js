abd63034b7c535686a693240f7d2c07fce83cddb
laynefaler <laynefaler@gmail.com>
2018-02-18 09:43:49 -0800

    Updates to middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,14 @@
 const connect = require('connect');
+const {...security} = require('./security');
+
+// Always last
 const {...errorHandling} = require('./errorHandling');
 
 module.exports = (function () {
     let chain = connect();
     Object.values({
         // list of middleware: order matters
+        ...security,
         ...errorHandling,
     }).forEach(e => {
         chain.use(e);
