4f349bf6f810ef1caacb067d5686a5db44564018
laynefaler <laynefaler@gmail.com>
2018-02-17 18:46:22 -0800

    Updates to linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
     "extends": "standard",
     "root": true,
-    "rules": {}
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "rules": {
+        "semi": [2, "always"],
+        "comma-dangle": [2, "always-multiline"],
+        "no-process-env": "off",
+        "indent": ["error", 4]
+    }
 };
--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
-'use strict'
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    underscored: true,
-    classMethods: {
-      associate: function (models) {
-        // associations can be defined here
-      }
-    }
-  })
-  return User
-}
+    var User = sequelize.define('User', {
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+    }, {
+        underscored: true,
+        rejectOnEmpty: true,
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+            },
+        },
+    });
+    return User;
+};
