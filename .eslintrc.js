e0c07eede8be2de5586ff2e0270ea1b5c86e5b01
laynefaler <laynefaler@gmail.com>
2018-02-18 09:14:02 -0800

    Updates to scripts fo rdatabase

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
         "ecmaVersion": 7,
         "sourceType": "module",
         "ecmaFeatures": {
-            "jsx": true,
             "experimentalObjectRestSpread": true
         }
     },
