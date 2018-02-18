1605a52ea88f8a183b8b5b218f5722e7eaa59c26
laynefaler <laynefaler@gmail.com>
2018-02-18 10:19:04 -0800

    Setting up preview views

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,3 +1,29 @@
+const {
+    INTERNAL_SERVER_ERROR,
+} = require('../utils/index');
+
 module.exports = {
+
+    internalError: (error, req, res, next) => {
+        if (error) {
+            res.status(INTERNAL_SERVER_ERROR.status_code).render('preview', {
+                errHeaders: error.headers,
+                complete: error.complete,
+                httpVersion: error.httpVersion,
+                statusCode: error.statusCode,
+                statusMessage: error.statusMessage,
+                stack: error.stack,
+                host: req.host,
+                path: req.path,
+                method: req.method,
+                reqHeaders: req.headers,
+                originalUrl: req.originalUrl,
+                params: req.params,
+                query: req.query,
+            });
+        } else {
+            next();
+        }
+    },
 
 };
