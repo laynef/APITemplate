688dad90c04a6cc68f149b70a972b04581454eda
laynefaler <laynefaler@gmail.com>
2018-02-18 20:47:51 -0800

    Updates not found pug template

--- controllers/notfound.js
+++ controllers/notfound.js
@@ -4,6 +4,11 @@ module.exports = {
     notfound: (req, res) => {
         res.status(NOT_FOUND.status_code).render('notfound', {
             status_code: NOT_FOUND.status_code,
+            path: req.path,
+            protocol: req.protocol,
+            hostname: req.hostname,
+            originalUrl: req.originalUrl,
+            status_message: NOT_FOUND.status_meaning,
         });
     },
 };
