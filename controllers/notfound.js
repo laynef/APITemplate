e565a13e44f76da75dec0cb31428c2bb5df8f90e
laynefaler <laynefaler@gmail.com>
2018-02-18 21:26:41 -0800

    Not found controllers and views

--- controllers/notfound.js
+++ controllers/notfound.js
@@ -2,14 +2,38 @@ const {NOT_FOUND} = require('../utils/index');
 
 module.exports = {
     notfound: (req, res) => {
+        const {protocol, hostname, originalUrl, baseUrl} = req;
         res.status(NOT_FOUND.status_code).render('notfound', {
             status_code: NOT_FOUND.status_code,
-            path: req.path,
-            protocol: req.protocol,
-            hostname: req.hostname,
-            baseUrl: req.baseUrl,
-            originalUrl: req.originalUrl,
             status_message: NOT_FOUND.status_meaning,
+            header: 'Route Not Found',
+            cardData: [
+                {
+                    header: `Path: ${req.path} (${NOT_FOUND.status_meaning})`,
+                    data: [
+                        {
+                            key: 'url',
+                            value: `${protocol}://${hostname}${originalUrl}`,
+                        },
+                        {
+                            key: 'protocol',
+                            value: protocol,
+                        },
+                        {
+                            key: 'host name',
+                            value: hostname,
+                        },
+                        {
+                            key: 'base url',
+                            value: baseUrl,
+                        },
+                        {
+                            key: 'path',
+                            value: req.path,
+                        },
+                    ].map(e => ({...e, action: 'Searched'})),
+                },
+            ],
         });
     },
 };
