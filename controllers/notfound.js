8e87cca65579e509cdb5f2cdc030fb2ff72ecd6f
laynefaler <laynefaler@gmail.com>
2018-02-18 19:33:16 -0800

    Updates to styles

--- controllers/notfound.js
+++ controllers/notfound.js
@@ -2,6 +2,8 @@ const {NOT_FOUND} = require('../utils/index');
 
 module.exports = {
     notfound: (req, res) => {
-        res.status(NOT_FOUND.status_code).render('notfound');
+        res.status(NOT_FOUND.status_code).render('notfound', {
+            status_code: NOT_FOUND.status_code,
+        });
     },
 };
