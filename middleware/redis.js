15972f60b4fc90610bd81c9350b03ebd371931bf
laynefaler <laynefaler@gmail.com>
2018-02-19 11:15:31 -0800

    Updates to express sessions

--- middleware/redis.js
+++ middleware/redis.js
@@ -1,12 +1,13 @@
 const sess = require('express-session');
 const RedisStore = require('connect-redis')(sess);
 const redis = require('redis');
+const client = redis.createClient(process.env.REDIS_URL || 'redis://localhost:6381');
 
 module.exports = {
 
     session: sess({
         secret: 'secret',
-        store: new RedisStore({ client: redis.createClient(process.env.REDIS_URL || 'redis://localhost:6381'), disableTTL: true }),
+        store: new RedisStore({ client: client, disableTTL: true }),
         saveUninitialized: true,
         resave: false,
         name: 'NewProject',
